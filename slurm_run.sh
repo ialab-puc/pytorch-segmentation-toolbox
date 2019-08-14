63a609375c494e68a4163fab543517ccdb661e8c
ironcadiz <afcadiz@uc.cl>
2019-08-14 12:30:06 -0400

    updated slurm run

--- slurm_run.sh
+++ slurm_run.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=output/psp_%j.log         # Nombre del output (%j se reemplaza por el ID del trabajo)
 #SBATCH --error=output/err/psp_%j.err          # Output de errores (opcional)
 #SBATCH --ntasks=1                   # Correr 2 tareas
-#SBATCH --cpus-per-task= 8           # Numero de cores por tarea
+#SBATCH --cpus-per-task=8           # Numero de cores por tarea
 #SBATCH --distribution=cyclic:cyclic # Distribuir las tareas de modo ciclico
 #SBATCH --time=0-02:00:00            # Timpo limite d-hrs:min:sec
 #SBATCH --mem-per-cpu=12000mb         # Memoria por proceso
@@ -12,7 +12,7 @@
 #SBATCH --mail-user=afcadiz@uc.cl    # El mail del usuario
 #SBATCH --partition=ialab-high        # Se tiene que elegir una partición de nodos con GPU
 #SBATCH --gres=gpu:Titan-RTX:2                 # Usar 2 GPUs (se pueden usar N GPUs de marca especifica de la manera --gres=gpu:marca:N)
-
+#SBATCH --nodelist=scyll
 
 uname -a
 CS_PATH=$1
