a9822c3bd76df5394c80ba8810a4632e2fee6032
ironcadiz <afcadiz@uc.cl>
2020-01-21 14:55:22 -0300

    updated slurm run

--- slurm_run.sh
+++ slurm_run.sh
@@ -14,13 +14,13 @@
 #SBATCH --gres=gpu:4                 # Usar 2 GPUs (se pueden usar N GPUs de marca especifica de la manera --gres=gpu:marca:N)
 #SBATCH --nodelist=hydra
 
-uname -a
 CS_PATH=$1
+MODEL=pspnet
 LR=1e-2
 WD=5e-4
 BS=8
 STEPS=40000
-GPU_IDS=0,1
+GPU_IDS=0
 
-#variable ${LOCAL_OUTPUT} dir can save data of you job, after exec it will be upload to hadoop_out path 
-pyenv/bin/python3  train.py --data-dir ${CS_PATH} --random-mirror --random-scale  --gpu ${GPU_IDS} --learning-rate ${LR}  --weight-decay ${WD} --batch-size ${BS} --num-steps ${STEPS}
+python -m torch.distributed.launch --nproc_per_node=4 train.py --data-dir ${CS_PATH} --model ${MODEL} --random-mirror --random-scale --learning-rate ${LR}  --weight-decay ${WD} --batch-size ${BS} --num-steps ${STEPS} --restore-from ./dataset/MS_DeepLab_resnet_pretrained_init.pth --gpu ${GPU_IDS}
+#python train.py --data-dir ${CS_PATH} --model ${MODEL} --random-mirror --random-scale --restore-from ./dataset/MS_DeepLab_resnet_pretrained_init.pth --gpu ${GPU_IDS} --learning-rate ${LR}  --weight-decay ${WD} --batch-size ${BS} --num-steps ${STEPS}
